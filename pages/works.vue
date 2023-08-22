9e96e28dda284ecc9d96303558256531e0475ee7
kasatomorning <9549356+kasatomorning@users.noreply.github.com>
2023-08-22 16:40:46 +0900

    worksページを修正

--- pages/works.vue
+++ pages/works.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        <h1>works page</h1>
+    <div class="lg:pl-[19.5rem]">
+        <div class="max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:pr-16">
+            <div class="mx-auto max-w-sm my-[1rem] text-center">
+                <h1
+                    class="inline-block text-5xl sm:text-5xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200">
+                    Works</h1>
+            </div>
+            <h2 class="inline-block text-4xl sm:text-4xl relative border-none lg:-ml-2 lg:pl-2 m-2">
+                <NuxtLink to="/">ポートフォリオサイト</NuxtLink>
+            </h2>
+            <p class="my-[0.3em]">Nuxt3とTypeScript、Tailwind CSSで作られています。</p>
+            <h2 class="inline-block text-4xl sm:text-4xl relative border-none lg:-ml-2 lg:pl-2 m-2  whitespace-pre-wrap">
+                <a
+                    href="https://chrome.google.com/webstore/detail/twc-history/lfgglnbbamjafbonbjbfhcmphkakckki?hl=ja">Chrome拡張機能</a>
+            </h2>
+            <p class="my-[0.3em]">ツイキャスの視聴履歴を保存する機能を追加するChrome拡張機能</p>
+            <p class="my-[0.3em]">TypeScriptで作られています。</p>
+
+
+        </div>
     </div>
 </template>
