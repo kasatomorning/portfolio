e8c65637a02a6e86a962600d9486b0e471f74cfe
kasatomorning <9549356+kasatomorning@users.noreply.github.com>
2023-08-23 01:00:16 +0900

    defaultを編集

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <Navbar></Navbar>
-        <slot></slot>
+        <div class="max-w-[88rem] mx-auto px-4 sm:px-6 md:px-8">
+            <Navbar></Navbar>
+            <slot></slot>
+        </div>
     </div>
 </template>
