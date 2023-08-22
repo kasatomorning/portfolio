c66e75186eb6cb75df8ad23f45d21ce68fbaea2b
kasatomorning <9549356+kasatomorning@users.noreply.github.com>
2023-08-22 16:16:35 +0900

    indexページを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,27 @@
 <template>
-    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      <div>
-        <div class="text-xl font-medium text-black">testTailwindCSS</div>
-        <p class="text-gray-500">test</p>
-      </div>
+    <div class="lg:pl-[19.5rem]">
+        <div class="max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:pr-16">
+            <div class="mx-auto max-w-sm my-[1rem] text-center">
+                <h1
+                    class="inline-block text-5xl sm:text-5xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200">
+                    Links</h1>
+            </div>
+            <h2 class="inline-block text-4xl sm:text-4xl relative border-none lg:-ml-2 lg:pl-2 m-2">自己紹介</h2>
+            <p class="my-[0.3em]">筑波大学情報学群 情報科学類所属の笠島です。</p>
+            <p class="my-[0.3em]">現在大学4年生で、ネットワーク系の研究室に所属しています。</p>
+            <h2 class="inline-block text-4xl sm:text-4xl relative border-none lg:-ml-2 lg:pl-2 m-2">資格</h2>
+            <div class="p-4 mb-2 max-w-lg bg-white rounded-md shadow-lg">
+                <p>2021年3月</p>
+                <p class="text-gray-600">基本情報技術者試験 合格</p>
+            </div>
+            <div class="p-4 mb-2 max-w-lg bg-white rounded-md shadow-lg">
+                <p>2022年12月</p>
+                <p class="text-gray-600">応用情報技術者試験 合格</p>
+            </div>
+            <div class="p-4 mb-2 max-w-lg bg-white rounded-md shadow-lg">
+                <p>2022年10月</p>
+                <p class="text-gray-600">TOEIC 730点</p>
+            </div>
+        </div>
     </div>
 </template>
